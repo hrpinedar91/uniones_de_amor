c8b1fbbac032b60986f0cdd9960fbbc72bad0e82
Harold Pineda <68312816+hrpinedar91@users.noreply.github.com>
2023-12-20 17:47:26 -0500

    add google verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,12 @@ import Script from "next/script";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Amarres de amor",
+  title: "Uniones de amor para siempre",
   description:
     "¿Estás buscando el amor verdadero, pero no lo encuentras? ¿Quieres recuperar a tu ex, pero no sabes cómo? ¿Te sientes solo, triste o desesperado por tu situación sentimental? No te preocupes, tenemos la solución para ti. En Amarres de amor efectivos y rápidos te ofrecemos los mejores servicios de magia blanca, rituales, hechizos y conjuros para solucionar tus problemas de amor. Contamos con un equipo de expertos que te asesorarán y te guiarán paso a paso para lograr tus objetivos.",
+  verification: {
+    google: "your-verification-id",
+  },
 };
 
 export default function RootLayout({
@@ -19,20 +22,19 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <>
-        <Script
-          strategy="afterInteractive"
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-F4932KY45Z"
-        />
-        <Script>{`
+      <Script
+        strategy="afterInteractive"
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-F4932KY45Z"
+      />
+      <Script>{`
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'G-F4932KY45Z');
 `}</Script>
-      </>
+
       <body className={inter.className}>
         <main>
           <NavbarResponsive />
