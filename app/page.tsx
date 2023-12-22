932e7aa88eabbd6f57951873d737d16f62132ce3
Harold Pineda <68312816+hrpinedar91@users.noreply.github.com>
2023-12-22 09:56:49 -0500

    Update WhatsApp links in code

--- app/page.tsx
+++ app/page.tsx
@@ -111,7 +111,10 @@ export default function HomePage() {
             src="/Illustration_desk.svg"
             alt="Imagen principal"
           />
-          <Link className="w-full" href="https://wa.link/elzn18">
+          <Link
+            className="w-full"
+            href="https://api.whatsapp.com/send?phone=+3248267662&text=Hola,%20me%20gustar%C3%ADa%20saber%20si%20puedes%20ayudarme%20con%20un%20problema%20amoroso.%20He%20le%C3%ADdo%20que%20eres%20experto%20en%20uniones%20de%20amor%20y%20me%20gustar%C3%ADa%20hacer%20uno%20para%20recuperar%20a%20mi%20ex%20pareja.%20%C2%BFQu%C3%A9%20necesito%20para%20hacerlo?%20%C2%BFQu%C3%A9%20garant%C3%ADas%20me%20ofreces?%20%C2%BFCu%C3%A1nto%20cuesta%20tu%20servicio?%20Espero%20tu%20respuesta,%20gracias.%20%F0%9F%98%8A"
+          >
             <Button bg="044343">Agendar cita Whatsapp</Button>
           </Link>
         </div>
@@ -153,7 +156,10 @@ export default function HomePage() {
             respeto por las tradiciones. Con nosotros, recibirás el apoyo
             constante que necesitas para alcanzar tus objetivos espirituales.
           </p>
-          <Link className="w-full" href="https://wa.link/elzn18">
+          <Link
+            className="w-full"
+            href="https://api.whatsapp.com/send?phone=+3248267662&text=Hola,%20me%20gustar%C3%ADa%20saber%20si%20puedes%20ayudarme%20con%20un%20problema%20amoroso.%20He%20le%C3%ADdo%20que%20eres%20experto%20en%20uniones%20de%20amor%20y%20me%20gustar%C3%ADa%20hacer%20uno%20para%20recuperar%20a%20mi%20ex%20pareja.%20%C2%BFQu%C3%A9%20necesito%20para%20hacerlo?%20%C2%BFQu%C3%A9%20garant%C3%ADas%20me%20ofreces?%20%C2%BFCu%C3%A1nto%20cuesta%20tu%20servicio?%20Espero%20tu%20respuesta,%20gracias.%20%F0%9F%98%8A"
+          >
             <button className="bg-[#e4e4e4] rounded-xl w-full mt-4 md:mt-0 text-black p-2">
               Consulta Gratis
             </button>
