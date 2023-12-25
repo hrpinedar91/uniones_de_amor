2f1fc3fc6c79c397e0c33b49076685a6fd54bb2d
Harold Pineda <68312816+hrpinedar91@users.noreply.github.com>
2023-12-25 14:37:07 -0500

    Update Google Analytics and Google Tag Manager scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,13 +27,25 @@ export default function RootLayout({
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-F4932KY45Z"
       />
-      <Script>{`
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
 
-  gtag('config', 'G-F4932KY45Z');
-`}</Script>
+      {/*  Google Analitycs */}
+      <Script>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());    
+              gtag('config', 'G-F4932KY45Z');`}
+      </Script>
+
+      {/* Google Tag Manager */}
+      <Script>
+        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-WL6DQ84P');`}
+      </Script>
+      {/* End Google Tag Manager */}
 
       <body className={inter.className}>
         <main>
